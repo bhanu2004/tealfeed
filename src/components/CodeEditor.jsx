d56fe46a636d6be8b09da74f2f363c55d4f3ea76
bhanu2004 <bhanuprksh05@gmail.com>
2024-06-30 06:23:42 +0530

    horizontal text fixes

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import React, { useEffect, useRef, useState } from 'react'
-
+import './code.css'
 import Prism from 'prismjs'
 
 const sampleText = 
@@ -17,8 +17,9 @@ function App() {
 ReactDOM.render(<App />, document.getElementById("root"));`;
 
 function CodeEditor() {
-    const [text, setText] = useState(sampleText);
-    const [areaText, setAreaText] = useState(sampleText);
+    const [codeText, setCodeText] = useState(sampleText);
+    const [fieldText, setFieldText] = useState(sampleText);
+    const [textHeight, setTextHeight] = useState();
   
     const textareaRef = useRef(null);
     useEffect(()=>{
@@ -28,22 +29,26 @@ function CodeEditor() {
       const textarea = textareaRef.current;
       textarea.style.height = 'auto';
       textarea.style.height = `${textarea.scrollHeight}px`;
-    },[text]);
+      setTextHeight(textarea.offsetHeight);
+    },[codeText]);
+
     
   const handleChange = (event) => {
       let val = event.target.value;
-      setAreaText(val);
+      setFieldText(val);
     if(val.length>1 && val.charCodeAt(val.length-1)==10 ){
-      setText(event.target.value+'\n');
+      setCodeText(event.target.value+'\n');
     }
-    else setText(event.target.value);
+    else setCodeText(event.target.value);
   };
   return (
     <div className='w-full max-w-[600px] h-[500px] relative overflow-y-auto mx-4'>
-      <pre className='absolute top-0 w-full min-h-[200px]  m-0  shadow-md'>
-        <code className={`language-js m-0`}>{text}</code>
+      <pre className='absolute whitespace-pre-wrap top-0 w-full min-h-[200px]  m-0 code-block shadow-md'
+      style={{height: `${textHeight}px`}}
+      >
+        <code className={`language-js w-full inline-block codeclass m-0`}>{codeText}</code>
       </pre>
-      <textarea spellCheck='false' value={areaText} onChange={(e)=>{handleChange(e);console.log("called",e.target.value.charCodeAt(e.target.value.length-1))}}
+      <textarea spellCheck='false' value={fieldText} onChange={(e)=>{handleChange(e);console.log("called",e.target.value.charCodeAt(e.target.value.length-1))}}
        ref={textareaRef}
        style={{resize: 'none',WebkitTextFillColor: 'transparent', fontFamily:"Consolas, Monaco, 'Andale Mono', monospace"}} 
        className='w-full min-h-[200px]  absolute p-4  leading-normal over bg-transparent text-white outline-none'
